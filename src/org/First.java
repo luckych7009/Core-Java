9cf60eb553853681492e79b62e58f666627e0b3c
luckych7009 <himanshuchaudhary7009@gmail.com>
2024-07-28 12:04:13 +0530

    FIst Java application

---
+++ src/org/First.java
@@ -0,0 +1,15 @@
+package org;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=4;
+		int b=5;
+		int sum=a+b;
+		System.out.println("The value of sum ="+sum);
+		
+	}
+
+}
