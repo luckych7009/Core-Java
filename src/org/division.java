c3fcbe49c69c3c0a00b6756b432bbb8cd688e847
luckych7009 <himanshuchaudhary7009@gmail.com>
2024-07-28 21:11:49 +0530

    created division  class

---
+++ src/org/division.java
@@ -0,0 +1,21 @@
+package org;
+
+public class division {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Use of division
+    int a=20;
+    int b=5;
+    int division=a/b;
+    System.out.println("division of 20 by 5 gives : "+division);
+    //Use of subtraction 
+    int x=150;
+	int y=250;
+	int subtraction=x-y;
+	System.out.println("subtraction of 250 from 150 : "+subtraction);
+
+    }
+	
+	}
+
